8eb38f74f1ef6e6355566c50b8983849dca9c792
Ratulvai <102589354+Ratulvai@users.noreply.github.com>
2022-03-29 16:01:48 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://Ratul:Ratul@cluster0.spsdd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5223289570:AAH6e6ni_CDCTvQzbRpsBVfkIu57KrOJQvA',//bot token
+    
+    bot_name:'freesolanaearningRobot',//bot username without @
+    
+    bot_admin:2021201644,// admin telegram id
+    
+    channelsList : ['@AirdropBean','@Inviteworkers_R','@AirdropInstractor','@AirdropCabinatee'],// channels list
+    
+    reffer_bonus:0.000008,// refferal bonus amount
+    
+    min_wd:0.000024,//min withdrawal amount
+    
+    daily_bonus:0.000008,// daily bonus amount
+    
+    currency:'Sol', //bot cuurency
+    
+    cb_api_key:'Q32wPDmirzJPPhQM',//coinbase api key
+    
+    cb_api_secret:'WwvMtuVf6Q7cdYGHC3PG4SEbhtPIRF2i',// coinbase secret key
+    
+    cb_account_id:'c835e309-f29b-517a-8f68-993f620fe7a5',// counbase account id for the bot currency
+    
+    payment_channel:'@allpayment2'// payment channel username
+    
+    }
